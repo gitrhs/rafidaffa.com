b135f67cc13c5a73dfeb88d243b7781b33f9b4ca
gitrhs <48622808+gitrhs@users.noreply.github.com>
2025-01-22 18:13:31 +0800

    modified:   content/course.php modified:   js/course.js modified:   main.css

--- content/course.php
+++ content/course.php
@@ -1,1 +1,55 @@
-course
+<div class="d-flex justify-content-center">
+    <div class="bigcontent">
+        <div class="d-flex align-items-center">
+            <taken-icon class="menu-icon-data"></taken-icon>&nbsp;Course & Certificate
+        </div>
+        <span class="text-silent subtitle">Here are some courses and certificates that I have taken</span>
+        <br>
+        <div class="horizontal">
+            <a class="menu-item course-item active" id="allcourse">
+                <span>All</span>
+            </a>
+            <a class="menu-item course-item" id="uni">
+                <span>University</span>
+            </a>
+            <a class="menu-item course-item" id="othercourse">
+                <span>Other Course</span>
+            </a>
+            <a class="menu-item course-item" id="doc">
+                <span>Cert and Doc</span>
+            </a>
+        </div>
+        <div class="menu-item-content">
+            <div id="allcourse-content" class="row text-center">
+                <br>
+                <div class="horizontal">
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>AI</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Python</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Database</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Core Course</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Elective Course</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>University Course</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Language Course</span>
+                    </button>
+                    <button type="button" class="btn btn-badge badge-secondary d-flex align-items-center">
+                        <span>Co-Curriculum</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <br><br>
+    </div>
+</div>
--- js/course.js
+++ js/course.js
@@ -1,1 +1,20 @@
-function coursefunc() {}
+function coursefunc() {
+    /*course menu*/
+    const menuItems = document.querySelectorAll(".menu-item");
+    const contentSections = document.querySelectorAll(
+        ".menu-item-content > div"
+    );
+
+    menuItems.forEach((item) => {
+        item.addEventListener("click", () => {
+            menuItems.forEach((menu) => menu.classList.remove("active"));
+            item.classList.add("active");
+            contentSections.forEach((section) =>
+                section.classList.add("hidden")
+            );
+            const contentId = `${item.id}-content`;
+            document.getElementById(contentId).classList.remove("hidden");
+        });
+    });
+    /*course menu ends*/
+}

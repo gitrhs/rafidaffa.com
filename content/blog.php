52d512204eb87c9ae81155285a87929bb530181d
gitrhs <48622808+gitrhs@users.noreply.github.com>
2025-02-05 22:19:38 +0800

    new file:   content/blog.php modified:   main.css

---
+++ content/blog.php
@@ -0,0 +1,18 @@
+<div class="row" style="overflow: hidden !important; height: 99vh; width: 100%;">
+    <div class="blog-navbar">
+        <div class="sticky-top d-flex align-items-center">
+            <div class="burger-icon">
+                <button type="button" class="btn btn-main d-flex justify-content-between align-items-center"
+                    onclick="burgerToogle()">
+                    <menu-icon class="burger-icon-data"></menu-icon>
+                </button>
+            </div>
+        </div>
+        <div style="position: absolute; top: 0; right: 0; padding: 15px;">
+            mail
+        </div>
+    </div>
+    <div class="col" style="overflow-y: auto !important; height: 100vh; background-image: radial-gradient(circle at 1px 1px, #a7a7a7 1px, transparent 0);
+  background-size: 25px 25px;">
+    </div>
+</div>

57fdde56a79faadd23b5e4ad105376e04e037e28
gitrhs <48622808+gitrhs@users.noreply.github.com>
2025-02-06 01:05:15 +0800

    modified:   content/blog.php modified:   content/home.php modified:   icon.js modified:   main.css

--- content/blog.php
+++ content/blog.php
@@ -1,15 +1,25 @@
 <div class="row" style="overflow: hidden !important; height: 99vh; width: 100%;">
     <div class="blog-navbar">
-        <div class="sticky-top d-flex align-items-center">
-            <div class="burger-icon">
-                <button type="button" class="btn btn-main d-flex justify-content-between align-items-center"
-                    onclick="burgerToogle()">
-                    <menu-icon class="burger-icon-data"></menu-icon>
+        <div class="sticky-top d-flex align-items-center" style="padding-left: 15px; padding-right: 5px;">
+            <div class=" d-flex justify-content-between align-items-center" style="width: 100%;">
+                <div class="d-flex align-items-center" style="margin-top: -3px;">
+                    <div class=" burger-icon">
+                        <button type="button" class="btn btn-main d-flex justify-content-between align-items-center"
+                            onclick="burgerToogle()">
+                            <menu-icon class="burger-icon-data"></menu-icon>
+                        </button>
+                    </div>
+                    <span><b>Blog</b></span>
+                </div>
+                <button type="button" class="btn btn-main d-flex align-items-center button-border"
+                    style="height: 30px;">
+                    <span class="menu-icon">
+                        <stream-icon class="menu-icon-data">
+                        </stream-icon>
+                    </span>
+                    <span style="margin-top: -2px;">Subscribe</span>
                 </button>
             </div>
-        </div>
-        <div style="position: absolute; top: 0; right: 0; padding: 15px;">
-            mail
         </div>
     </div>
     <div class="col blog-content idle">

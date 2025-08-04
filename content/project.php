d5a607435d305d70c90b9c4a73223aa47ea4c009
gitrhs <48622808+gitrhs@users.noreply.github.com>
2025-08-04 20:16:14 +0800

    feat(projects): update project images and contributor profiles
    - Replace placeholder images with actual contributor profile photos
    - Add new project images for KTMB and NextWord projects
    - Update project links and details for better navigation

--- content/project.php
+++ content/project.php
@@ -29,11 +29,11 @@
                                     <div class="project-title">
                                         Senarafest</div>
                                     <div class="contributor d-flex">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
-                                            class="contributor-img" style="margin-right: -10px;">
+                                        <img src="image/profile/malva.jpg" class="contributor-img"
+                                            style="margin-right: -10px;">
                                         <img src="image/profile/zikra.jpg" class="contributor-img"
                                             style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -54,7 +54,8 @@
                                 <div class="project-image-container">
                                     <img src="image/project/senarafest.png">
                                     <div class="image-overlay">
-                                        <a href="" target="_blank" class="anone badge overlay-btn">Website</a>
+                                        <a href="https://senarafest.com" target="_blank"
+                                            class="anone badge overlay-btn">Website</a>
                                         <a href="" class="anone badge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
@@ -70,7 +71,7 @@
                                     <div class="contributor d-flex">
                                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
                                             class="contributor-img" style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -109,7 +110,7 @@
                                             class="contributor-img" style="margin-right: -10px;">
                                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
                                             class="contributor-img" style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -153,7 +154,7 @@
                                     <div class="contributor d-flex">
                                         <img src="image/profile/misbah.jpg" class="contributor-img"
                                             style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -175,7 +176,7 @@
                                 <div class="project-image-container">
                                     <img src="image/project/avatarai.png">
                                     <div class="image-overlay">
-                                        <a href="" class="anone adge overlay-btn">Project Details</a>
+                                        <a href="avatarai" class="anone adge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
                             </div>
@@ -190,7 +191,7 @@
                                     <div class="contributor d-flex">
                                         <img src="image/profile/misbah.jpg" class="contributor-img"
                                             style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -212,7 +213,7 @@
                                 <div class="project-image-container">
                                     <img src="image/project/sts.png">
                                     <div class="image-overlay">
-                                        <a href="" class="anone badge overlay-btn">Project Details</a>
+                                        <a href="sts" class="anone badge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
                             </div>
@@ -225,9 +226,9 @@
                                     <div class="project-title">
                                         Whisper of the Path</div>
                                     <div class="contributor d-flex">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
-                                            class="contributor-img" style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/alyssa.jpg" class="contributor-img"
+                                            style="margin-right: -10px;">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -260,7 +261,7 @@
                                     <div class="project-title">
                                         PPIM Calendar</div>
                                     <div class="contributor d-flex">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -281,7 +282,83 @@
                                 <div class="project-image-container">
                                     <img src="image/project/calendar.png">
                                     <div class="image-overlay">
-                                        <a href="" target="_blank" class="anone badge overlay-btn">Website</a>
+                                        <a href="https://calendar.ppimalaysia.id" target="_blank"
+                                            class="anone badge overlay-btn">Website</a>
+                                        <a href="" class="anone badge overlay-btn">Project Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl-4" style="margin-bottom: 25px;">
+                        <div class="project-card d-flex align-items-center">
+                            <div>
+                                <div class="project-header d-flex align-items-end justify-content-between">
+                                    <div class="project-title">
+                                        Word Autocomplete</div>
+                                    <div class="contributor d-flex">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
+                                            class="contributor-img" style="margin-right: -10px;">
+                                        <img src="image/profile/harishankar.jpg" class="contributor-img"
+                                            style="margin-right: -10px;">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
+                                    </div>
+                                </div>
+                                <div class="text-silent project-desc">
+                                    A machine learning that predict next word
+                                </div>
+                                <div class="project-details">
+                                    <div class="hr0-10"></div>
+                                    <p class="project-details-short-desc">
+                                        Interactive art exhibition platform with artist portfolios and event
+                                        management.
+                                    </p>
+                                    <div class="tech-stack">
+                                        <span class="badge blog-badge badge-blue">Python</span>
+                                        <span class="badge blog-badge badge-blue">Flask</span>
+                                        <span class="badge blog-badge badge-blue">HTML</span>
+                                        <span class="badge blog-badge badge-blue">CSS3</span>
+                                        <span class="badge blog-badge badge-blue">Javascript</span>
+                                    </div>
+                                </div>
+                                <div class="project-image-container">
+                                    <img src="image/project/nextword.png">
+                                    <div class="image-overlay">
+                                        <a href="" target="_blank" class="anone badge overlay-btn">Github Page</a>
+                                        <a href="" class="anone badge overlay-btn">Project Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl-4" style="margin-bottom: 25px;">
+                        <div class="project-card d-flex align-items-center">
+                            <div>
+                                <div class="project-header d-flex align-items-end justify-content-between">
+                                    <div class="project-title">
+                                        KTMB Dynamic Fare</div>
+                                    <div class="contributor d-flex">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
+                                            class="contributor-img" style="margin-right: -10px;">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
+                                    </div>
+                                </div>
+                                <div class="text-silent project-desc">
+                                    ML-based KTMB fare optimization
+                                </div>
+                                <div class="project-details">
+                                    <div class="hr0-10"></div>
+                                    <p class="project-details-short-desc">
+                                        Interactive art exhibition platform with artist portfolios and event
+                                        management.
+                                    </p>
+                                    <div class="tech-stack">
+                                        <span class="badge blog-badge badge-blue">Python</span>
+                                    </div>
+                                </div>
+                                <div class="project-image-container">
+                                    <img src="image/project/ktmb.png">
+                                    <div class="image-overlay">
                                         <a href="" class="anone badge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
@@ -295,9 +372,9 @@
                                     <div class="project-title">
                                         UM Notes</div>
                                     <div class="contributor d-flex">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
-                                            class="contributor-img" style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/nikan.jpg" class="contributor-img"
+                                            style="margin-right: -10px;">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -319,8 +396,9 @@
                                 <div class="project-image-container">
                                     <img src="image/project/umnotes.png">
                                     <div class="image-overlay">
-                                        <a href="" class="anone adge overlay-btn">Website</a>
-                                        <a href="" class="anone adge overlay-btn">Project Details</a>
+                                        <a href="https://notes.ppiunimalaya.id" target="_blank"
+                                            class="anone adge overlay-btn">Website</a>
+                                        <a href="umnotes" class="anone adge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
                             </div>
@@ -335,7 +413,7 @@
                                     <div class="contributor d-flex">
                                         <img src="image/profile/keiko.jpg" class="contributor-img"
                                             style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -357,8 +435,9 @@
                                 <div class="project-image-container">
                                     <img src="image/project/poernamafest.png">
                                     <div class="image-overlay">
-                                        <a href="" target="_blank" class="anone badge overlay-btn">Website Demo</a>
-                                        <a href="" class="anone badge overlay-btn">Project Details</a>
+                                        <a href="https://poernamafest.rafidaffa.com" target="_blank"
+                                            class="anone badge overlay-btn">Website Demo</a>
+                                        <a href="poernamafest" class="anone badge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
                             </div>
@@ -371,9 +450,9 @@
                                     <div class="project-title">
                                         Poernamafest Store</div>
                                     <div class="contributor d-flex">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbuCSYchUiacL_rMjjYa-QOAzI4CFVeqYnT4Oa7t2pdcUsTXg0BQjGqqgWgKs35rHhF6c&usqp=CAU"
-                                            class="contributor-img" style="margin-right: -10px;">
-                                        <img src="image/profile/rafi.jpg" class="contributor-img">
+                                        <img src="image/profile/bimo.jpg" class="contributor-img"
+                                            style="margin-right: -10px;">
+                                        <img src="image/profile/rafi2.jpg" class="contributor-img">
                                     </div>
                                 </div>
                                 <div class="text-silent project-desc">
@@ -394,8 +473,9 @@
                                 <div class="project-image-container">
                                     <img src="image/project/poernamastore.png">
                                     <div class="image-overlay">
-                                        <a href="" target="_blank" class="anone badge overlay-btn">Website Demo</a>
-                                        <a href="" class="anone badge overlay-btn">Project Details</a>
+                                        <a href="https://poernamastore.rafidaffa.com" target="_blank"
+                                            class="anone badge overlay-btn">Website Demo</a>
+                                        <a href="poernamafeststore" class="anone badge overlay-btn">Project Details</a>
                                     </div>
                                 </div>
                             </div>

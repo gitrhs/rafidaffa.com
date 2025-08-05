067f599c30a294058df2ebbcc592ca775a7e7064
gitrhs <48622808+gitrhs@users.noreply.github.com>
2025-08-06 01:03:58 +0800

    feat: add favicon to all pages and style burger button
    Add favicon to improve brand recognition and user experience
    Style burger button with rounded corners for better visual appeal

--- blog-temp.php
+++ blog-temp.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="main.css?dev=<?php echo $version; ?>" />
+    <link rel="icon" type="image/png" href="icon/rafi-ico2.png">
     <script src="icon.js?dev=<?php echo $version; ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.172.0/three.tsl.js"
         integrity="sha512-PiFiRajtnAStBKn2yo9z+RO3RJ6/Njc4l2KhPRuJ3jQ3J8aro4NRQpoGVNDOJNQ6Iuzou85TWxN2QGNDUyj6wA=="
